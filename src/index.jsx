b88010398c6e29a96da7f35f0a7ab5ba2c7affc0
iaffs <byiaffs@gmail.com>
2020-05-14 12:01:51 +0200

    added routes and notfound function

--- src/index.jsx
+++ src/index.jsx
@@ -13,12 +13,40 @@ class App extends Component {
         };
     }
 
+    notFound = () => {
+        return(
+            <H1>not found</H1>
+        )
+    }
+
     render() {
         return(
 
             <BrowserRouter>
                 <div>
                     <HeaderBar></HeaderBar>
+                    <Switch>
+                        <Route exact path="/" 
+                        render={props => <Home {...props}></Home>}>
+                        </Route>
+                        <Route exact path="/login" 
+                        render={props => <Login {...props}></Login>}>
+                        </Route>
+                        <Route exact path="/signup" 
+                        render={props => <SignUp {...props}></SignUp>}>
+                        </Route>
+                        <Route exact path="/userprofile" 
+                        render={props => <UserProfile {...props}></UserProfile>}>
+                        </Route>
+                        <Route exact path="/userdetails" 
+                        render={props => <UserDetails {...props}></UserDetails>}>
+                        </Route>
+                        <Route exact path="/tickets" 
+                        render={props => <Tickets {...props}></Tickets>}>
+                        </Route>
+                        <Route component={this.notFound}>
+                        </Route>
+                    </Switch>
                 </div>
             </BrowserRouter>
         )
