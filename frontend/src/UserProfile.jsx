175cb0c3c064a5f9daec7c09d4d7514bad8cc847
Marcin Janecki <Amish1188@MacBook-Pro-Marcin.local>
2020-05-14 12:56:40 +0200

    add text

--- frontend/src/UserProfile.jsx
+++ frontend/src/UserProfile.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 class UserProfile extends Component {
-    state = {  }
+    constructor(props){
+        super(props);
+        this.state = {
+            //I create fake user data for now
+            user = {
+                name: "Jonathan Swallow",
+                
+            }
+        }
+    }
     render() {
         return (
             <div>profile</div>
