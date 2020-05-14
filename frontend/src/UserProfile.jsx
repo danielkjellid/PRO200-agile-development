b2c43fc7cd461f9dffab92983861a097b225f0bf
Marcin Janecki <Amish1188@MacBook-Pro-Marcin.local>
2020-05-14 16:01:11 +0200

    add BuySingleTicketBS

--- frontend/src/UserProfile.jsx
+++ frontend/src/UserProfile.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import ChooseTicketBS from './bottomsheets/ChooseTicketBS';
 
 class UserProfile extends Component {
     constructor(props){
@@ -8,19 +9,42 @@ class UserProfile extends Component {
             //I create fake user data for now
             user : {
                 name: "Jonathan Swallow",    
-            }
+            },
+            chooseTicket: false
         }
     }
+
+    newTicketHandler = () => {
+        if(this.state.chooseTicket === false){
+            this.setState({chooseTicket: true});
+        } else {
+            this.setState({chooseTicket: false});
+        }
+    }
+
+    renderChooseTicket = () => {
+        if(this.state.chooseTicket) {
+            return (
+                <React.Fragment>
+                    <ChooseTicketBS></ChooseTicketBS>
+                </React.Fragment>
+            )
+        }
+    }
+
+
     render() {
         return (
             <div>
                 <h1>Hei, {this.state.user.name}</h1>
-                <button>Ny billett</button>
+                <button onClick={this.newTicketHandler}>Ny billett</button>
 
                 <h3>Billetter</h3>
                 <Link to={'/tickets'}>Se alle billetter</Link>
 
                 <div>Miljøkalkulator</div>
+
+                {this.renderChooseTicket()}
             </div>
         );
     }
