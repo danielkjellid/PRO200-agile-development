12e3ff0b6d314fb2fafc2e9d9f764c3249949ad7
Julie Marie Mamelund <julie@mamelund.no>
2020-05-14 18:24:02 +0200

    Init backend w/models

---
+++ backend/Models/Entity.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace VyShare.Models
+{
+    public class Entity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ backend/Models/Person.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace VyShare.Models
+{
+    public class Person : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Name => FirstName + " " + LastName;
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
