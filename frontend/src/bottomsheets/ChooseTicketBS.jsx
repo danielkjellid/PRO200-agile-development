08de5629998712479b49402b2069b422636b7317
Ludvik <luddmachine@gmail.com>
2020-05-20 15:57:56 +0200

    added styling to ChooseTicketBS.jsx using tailwind classes

--- frontend/src/bottomsheets/ChooseTicketBS.jsx
+++ frontend/src/bottomsheets/ChooseTicketBS.jsx
@@ -3,19 +3,6 @@ import React, { Component } from 'react';
 class ChooseTicketBS extends Component {
     render() {
 
-        const modalContainer = {
-            width: "100%",
-            height: "40vh",
-            padding: "10px",
-            boxSizing: "border-box",
-            display: "flex",
-            flexDirection: "column",
-            bottom: "0%",
-            position: "fixed",
-            zIndex: 2,
-            backgroundColor: "white"
-        }
-
         const modalBack = {
             width: "100%",
             height: "100vh",
@@ -25,26 +12,29 @@ class ChooseTicketBS extends Component {
             opacity: "0.5"
         }
 
-        const button = {
-            boxSizing: "border-box",
-            padding: "40px 40px",
-            border: "solid 1px #E2E8F0",
-            backgroundColor: "white"
-        }
-
         return (
             <div>
-                <div style={modalContainer}>
-                    <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
-                        <h4>Velg billett</h4>
-                        <button onClick={this.props.clickX}>x</button>
+                <div className="w-full z-10 absolute bottom-0 h-auto bg-white rounded-t-md rounded-b-lg">
+                    <div className="flex flex-row justify-between p-5 border-b border-grey-300 mb-5">
+                        <h4 className="font-medium">Velg billett</h4>
+                        <button onClick={this.props.clickX}>
+                            <svg className="h-5 w-5 text-gray-600" fill="currentColor" viewBox="0 0 20 20"><path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" fillRule="evenodd"/></svg>
+                        </button>
                     </div>
-                    <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
-                        <button style={button} id="single" onClick={this.props.click}>Enkeltbillett</button>
-                        <button  style={button} id="period">Periodebillett</button>
+                    <div className="flex flex-row justify-between pr-5 pl-5 pb-5">
+                        <button className="p-8 border border-gray-300 rounded-md font-medium text-gray-700" id="single" onClick={this.props.click}>Enkeltbillett</button>
+                        <button className="p-8 border border-gray-300 rounded-md font-medium text-gray-700" id="period">Periodebillett</button>
                     </div>
-                    <p>Ekstra soner</p>
-                    <p>Kjøp til andre</p>
+                    <div className="pl-5 pr-5 pb-24">
+                        <div className="pb-5 border-b border-grey-300">
+                            <p className="font-medium text-sm text-gray-700">Ekstra soner</p>
+                            <p className="font-normal text-gray-600 text-xs">Kjøp billett til andre soner</p>
+                        </div>
+                        <div className="pt-5">
+                            <p className="font-medium text-sm text-gray-700">Kjøp til andre</p>
+                            <p className="font-normal text-gray-600 text-xs">Send billetten til en annen telefon</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
