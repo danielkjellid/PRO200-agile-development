d914ab739607ce5f433c9eec56dc50ab23f22034
iaffs <byiaffs@gmail.com>
2020-05-20 14:00:58 +0200

    made fakeData boughtTickets

--- frontend/src/bottomsheets/sendTicketsBS.jsx
+++ frontend/src/bottomsheets/sendTicketsBS.jsx
@@ -1,2 +1,11 @@
 import React, { Component } from 'react';
 
+class sendTicketBS extends Component {
+    this.state = {
+        reviewTicketsShow: true,
+        contactListShow: false,
+        sentTicketsConfirmationShow: false
+    }
+
+    
+}
---
+++ frontend/src/fakeData/boughtTicket.js
@@ -0,0 +1,16 @@
+
+const boughtTickets = [
+    {
+        ticket: "Barnebillett",
+        passiveTicket: 1,
+        activeTicket: 0
+    },
+    {
+        ticket: "Voksenbillett",
+        passiveTicket: 2,
+        activeTicket: 0
+    }
+
+]
+
+export default boughtTickets;
