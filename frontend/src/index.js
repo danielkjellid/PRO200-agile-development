97bdd72280bf0c98029ba6a8ac48e27c6d7a6171
danielkjellid <daniel.kjellid@gmail.com>
2020-05-20 14:25:03 +0200

    Moved css files and tailwind build process to public directory

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './tailwind.generated.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
