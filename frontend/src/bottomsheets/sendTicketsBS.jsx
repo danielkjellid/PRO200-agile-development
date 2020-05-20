f4d7a4d14a720ccef918951539567b032c250bdb
iaffs <byiaffs@gmail.com>
2020-05-20 14:37:32 +0200

    sendTicket method and rendering

--- frontend/src/bottomsheets/sendTicketsBS.jsx
+++ frontend/src/bottomsheets/sendTicketsBS.jsx
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
+import boughtTickets from "../fakeData/boughtTicket";
 
 class sendTicketBS extends Component {
-    this.state = {
-        reviewTicketsShow: true,
-        contactListShow: false,
-        sentTicketsConfirmationShow: false
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            reviewTicketsShow: true,
+            contactListShow: false,
+            sentTicketsConfirmationShow: false,
+            boughtTickets: boughtTickets,
+            renderButtonText: "Send billetter"
+        }
     }
 
+    reviewTicket = () => {
+        if (this.state.reviewTicketsShow) {
+            return(
+                <React.Fragment>
+                    <div>
+                        {this.state.boughtTickets.map(item => {
+                            return(
+                            <div>
+                                {item.ticket} 
+                                {item.activeTicket} 
+                                {item.passiveTicket}
+                            </div>)
+                            })}
+                    </div>
+                </React.Fragment>
+            )
+        }
+        
+    }
     
+
+    render() {
+
+        return(
+            <div className="modalSendTickets">
+                <p>Send billetter</p>
+                <button>x</button>
+                {this.reviewTicket()}
+                <button className="fortsettButton fortsettButtonDisabled">{this.state.renderButtonText}</button>
+            </div>
+        )
+    }
+
+
 }
+
+export default sendTicketBS;
