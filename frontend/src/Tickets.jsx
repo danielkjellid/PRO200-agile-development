56de0f5f1241f3d6daf06e03d07dcdddd35fb12d
amish1188 <martin.joodd@gmail.com>
2020-05-21 12:07:17 +0200

    developed Tickets component

--- frontend/src/Tickets.jsx
+++ frontend/src/Tickets.jsx
@@ -5,6 +5,8 @@ class Tickets extends Component {
     this.state = {
       isLoaded: true,
       orders: [],
+      areTicketsLoaded: true,
+      ticketsByOrder: []
     };
   }
 
@@ -13,7 +15,6 @@ class Tickets extends Component {
       const response = await fetch("https://localhost:5001/orders", {method: "get"});
       
       const payload = await response.json();
-      console.log(payload);
       this.setState({
         orders: payload,
         isLoaded: false,
@@ -23,15 +24,50 @@ class Tickets extends Component {
     }
   }
 
+  fetchTicketsByOrder = async (id) => {
+    try {
+      const response = await fetch(
+        `https://localhost:5001/orders/${id}/basictickets`
+      );
+      const payload = await response.json();
+      this.setState({
+        ticketsByOrder: payload
+      })
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  
+  showTicketsByOrder = (id) => {
+      this.fetchTicketsByOrder(id)
+    if (this.state.ticketsByOrder > 0) { this.renderTicketsByOrder() }
+  }
+  
+  renderTicketsByOrder = () => {
+    
+        return (this.state.ticketsByOrder.map((item) => {
+          return (
+            <div>
+              <p>{item.type}</p>
+              <p>{item.startPoint}</p>
+              <p>{item.endPoint}</p>
+              <p>{item.price}</p>
+            </div>
+          )
+        }))
+      
+  }
+  
   render() {
     if (this.state.isLoaded) {
       return <div>Is Loading</div>;
     } else {
       return (this.state.orders.map((item) => {
-        return (<p>{item.name}</p>)
-      }))
+        return (<div><p key={item.id} onClick={() => this.showTicketsByOrder(item.id)}>
+          {item.name}</p><div>{this.renderTicketsByOrder()}</div></div>)
+      }
+      ))}
     }
-  }
 }
 
 export default Tickets;
