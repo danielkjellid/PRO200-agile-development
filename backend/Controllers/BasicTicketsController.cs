72a12bf6e59c8cc28568c751ece6ea1d60a7a4cb
Julie Marie Mamelund <julie@mamelund.no>
2020-05-22 17:37:18 +0200

    Easier ticket assignment

--- backend/Controllers/BasicTicketsController.cs
+++ backend/Controllers/BasicTicketsController.cs
@@ -53,10 +53,67 @@ namespace VyShare.Controllers
         }
 
 
+        [HttpPut("{ticketId}")]
+        public async Task<ActionResult> Update(Guid orderId, Guid ticketId, BasicTicketDto basicTicketDto)
+        {
+            var order = await db.Orders.Include(e => e.BasicTickets).FirstOrDefaultAsync(e => e.Id == orderId);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            var basicTicket = order.BasicTickets.FirstOrDefault(e => e.Id == ticketId);
+            if (basicTicket == null)
+            {
+                return NotFound();
+            }
+
+            basicTicket = basicTicketDto.ToBasicTicket(db, basicTicket);
+            await db.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPost("{ticketId}/assignto/{personId}")]
+        public async Task<ActionResult> AssignTicketToPerson(Guid orderId, Guid ticketId, Guid personId)
+        {
+            var order = await db.Orders
+                .Include(e => e.BasicTickets)
+                    .ThenInclude(e => e.TicketHolder)
+                .FirstOrDefaultAsync(e => e.Id == orderId);
+            if (order == null)
+            {
+                return NotFound("Order not found");
+            }
+
+            var basicTicket = order.BasicTickets.FirstOrDefault(e => e.Id == ticketId);
+            if (basicTicket == null)
+            {
+                return NotFound("Ticket not found");
+            }
+
+            if(basicTicket.TicketHolder != null){
+                return BadRequest("Ticket is already assigned to another person");
+            }
+
+            var person = await db.People.FindAsync(personId);
+            if (person == null)
+            {
+                return NotFound("Person not found");
+            }
+
+            basicTicket.TicketHolder = person;
+            await db.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
         [Route("/contacts/{id}/tickets")]
         [Route("/users/{id}/tickets")]
         [HttpGet]
-        public async Task<ActionResult<List<BasicTicketDto>>> GetPersonTickets(Guid id){
+        public async Task<ActionResult<List<BasicTicketDto>>> GetPersonTickets(Guid id)
+        {
             var tickets = await db.BasicTickets
                 .Include(e => e.TicketHolder)
                 .Where(e => e.TicketHolder.Id == id).ToListAsync();
