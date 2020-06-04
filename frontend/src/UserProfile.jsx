8855a80890563578a4111df187cb2cca28e1a7ec
amish1188 <martin.joodd@gmail.com>
2020-06-04 11:47:07 +0200

    add BuyNewTicket component

--- frontend/src/UserProfile.jsx
+++ frontend/src/UserProfile.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import ChooseTicketBS from './bottomsheets/ChooseTicketBS';
-import BuySingleTicketBS from './bottomsheets/BuySingleTicketBS';
-import SendTicketBS from './bottomsheets/SendTicketsBS';
+
 
 import UserHeader from './components/UserHeader';
 import TicketList from './components/TicketList'
@@ -12,105 +10,21 @@ class UserProfile extends Component {
 		this.state = {
 			//I create fake user data for now
 			name: '',
-			contactList: [],
-			chooseTicket: false,
-			singleTicket: false,
-			periodTicket: false,
-			sendTicketShow: false,
 			activeTicket: true,
 		};
 
-		this.newTicketButtonHandler = this.newTicketButtonHandler.bind(this);
+		
 	}
 
-	newTicketButtonHandler = () => {
-		const newTicketShow = this.state.chooseTicket;
-
-		if (newTicketShow) {
-			this.setState({ chooseTicket: false });
-			this.props.endTransaction();
-		} else {
-			this.setState({ chooseTicket: true });
-			this.props.fadeBackground();
-		}
-	};
-
-	buySingleTicketButtonHandler = () => {
-		const buySingleTicketShow = this.state.singleTicket;
-		this.setState({ singleTicket: !buySingleTicketShow });
-		this.newTicketButtonHandler();
-		this.props.fadeBackground();
-	};
-
-	renderChooseTicket = () => {
-		if (this.state.chooseTicket) {
-			return (
-				<ChooseTicketBS
-					click={this.buySingleTicketButtonHandler}
-					clickX={this.newTicketButtonHandler}
-				/>
-			);
-		}
-	};
-
-	renderBuySingleTicket = () => {
-		if (this.state.singleTicket) {
-			return (
-				<BuySingleTicketBS
-					endTransaction={this.endTransaction}
-					hideBuySingleTicket={this.hideBuySingleTicket}
-					renderSendTicket={this.sendTicketsHandler}
-				/>
-			);
-		}
-	};
-
-	sendTicketsHandler = () => {
-		this.setState({ sendTicketShow: true });
-		this.setState({ singleTicket: false });
-	};
-
-	renderSendTicket = () => {
-		let contactList;
-		if (this.state.contactList) {
-			contactList = this.state.contactList;
-		}
-		if (this.state.sendTicketShow) {
-			return (
-				<SendTicketBS
-					updateContactList={this.props.updateContactList}
-					contactList={this.props.contactList}
-					endTransaction={this.props.endTransaction}
-					endSendingTickets={this.endSendingTickets}
-				/>
-			);
-		}
-	};
-
-	hideBuySingleTicket = () => {
-		this.endTransaction();
-		this.newTicketButtonHandler();
-	};
-
-	endTransaction = () => {
-		this.setState({ singleTicket: false });
-		this.props.endTransaction();
-	};
-
-	endSendingTickets = () => {
-		this.setState({ sendTicketShow: false });
-		this.props.endTransaction();
-	};
+	
 
 	render() {
 		return (
 			<div>
-				{this.renderChooseTicket()} {/* change into component tag */} 
-				{this.renderBuySingleTicket()} {/* change into component tag */} 
-				{this.renderSendTicket()} {/* change into component tag */} 
+				
 				<UserHeader
 					userName={this.props.user}
-					buttonHandler={this.newTicketButtonHandler}
+					buttonHandler={this.props.newTicketButtonHandler}
 				/>
 				<TicketList
 					title="Aktiv billett"
