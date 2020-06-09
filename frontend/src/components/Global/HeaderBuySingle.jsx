5421230b2154a4f4d5eaaa6a19105f1cb8fe82bd
danielkjellid <daniel.kjellid@gmail.com>
2020-06-09 20:55:44 +0200

    Global components should now be indented. BuyNewTicket needs commenting

--- frontend/src/components/Global/HeaderBuySingle.jsx
+++ frontend/src/components/Global/HeaderBuySingle.jsx
@@ -8,7 +8,12 @@ function HeaderBuySingle(props) {
             {
                 !props.confirmation ? 
                     <React.Fragment>
-                        <svg className="cursor-pointer text-gray-700 w-5 h-5" onClick={props.back} fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd"/></svg>
+                        <svg 
+                            className="cursor-pointer text-gray-700 w-5 h-5" 
+                            onClick={props.back} fill="currentColor" viewBox="0 0 20 20"
+                        >
+                            <path fillRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd"/>
+                        </svg>
                         <p className="font-medium text-gray-900">Kjøp enkeltbillett</p>
                     </React.Fragment>
                     :
